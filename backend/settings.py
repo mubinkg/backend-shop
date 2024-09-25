439c6a1757e8303767c9b661901ab83d627c4af8
mubin <mubin.ice.ru@gmail.com>
2024-09-25 22:44:43 +0600

    udpate db

--- backend/settings.py
+++ backend/settings.py
@@ -128,16 +128,16 @@ DATABASES = {
 
 # Database
 # https://docs.djangoproject.com/en/5.0.6/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'HouseSteel',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'HouseSteel',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
