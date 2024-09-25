91d59c0bba4acb88821982d7a5d6d4483c1081de
mubin <mubin.ice.ru@gmail.com>
2024-09-26 02:20:34 +0600

    udpate static file

--- backend/settings.py
+++ backend/settings.py
@@ -176,7 +176,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
